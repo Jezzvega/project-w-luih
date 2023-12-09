dafd7bdc2ec22667a1b905c1e1a0a6bc921ac996
Emilio Vega <jesusvegamoreno@gmail.com>
2023-12-09 05:13:50 +0000

    feat: New functions\nBREAKING CHANGE: ahora todo cambio, le to ca a ella

--- src/luih/operations.py
+++ src/luih/operations.py
@@ -2,15 +2,15 @@ from time import sleep
 from luih.utils import get_cwd
 
 
-def add_two_numbers(a: int, b: int) -> int:
+def _add_two_numbers(a: int, b: int) -> int:
     sleep(5)
     return a + b
 
 
-def multiply_two_numbers(a: int, b: int) -> int:
+def _multiply_two_numbers(a: int, b: int) -> int:
     get_cwd()
     return a * b
 
 
-def divide_two_numbers(a: int, b: int) -> float:
+def _divide_two_numbers(a: int, b: int) -> float:
     return a / b
